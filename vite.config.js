11cbd51d3f20853f7e63b3aac13ead2664b8be69
Nevindi-BotCalm <nevind.l@botcalm.com>
2025-09-30 19:32:08 +0530

    Styles missing ADDED

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Portfolio/',
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-    outDir: 'dist'
-  }
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+ 
 })
